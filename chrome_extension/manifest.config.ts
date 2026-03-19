5ee8e74c05517e173a2c0a92bdb8cd0b309da7ec
Shakib Khan <leadwithshakib@gmail.com>
2026-03-19 21:58:36 +0600

    chore: update manifest permissions for audio capture

---
+++ chrome_extension/manifest.config.ts
@@ -0,0 +1,34 @@
+import { defineManifest } from '@crxjs/vite-plugin'
+import pkg from './package.json'
+
+export default defineManifest({
+  manifest_version: 3,
+  name: pkg.displayName,
+  description: pkg.description,
+  version: pkg.version,
+  icons: {
+    16: 'public/icons/icon16.png',
+    32: 'public/icons/icon32.png',
+    48: 'public/icons/icon48.png',
+    128: 'public/icons/icon128.png',
+  },
+  action: {
+    default_icon: {
+      16: 'public/icons/icon16.png',
+      32: 'public/icons/icon32.png',
+      48: 'public/icons/icon48.png',
+      128: 'public/icons/icon128.png',
+    },
+    default_popup: 'src/popup/index.html',
+  },
+  permissions: ['tabCapture', 'sidePanel', 'contentSettings', 'activeTab'],
+  content_scripts: [
+    {
+      js: ['src/content/main.tsx'],
+      matches: ['https://*/*'],
+    },
+  ],
+  side_panel: {
+    default_path: 'src/sidepanel/index.html',
+  },
+})
